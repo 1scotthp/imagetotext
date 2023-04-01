e23b89f0c08ebb705b4c81b1c3fdf3ddaf80720b
1scotthp <51714937+1scotthp@users.noreply.github.com>
2023-04-01 12:44:18 -0700

    first

---
+++ app.py
@@ -0,0 +1,38 @@
+from flask import Flask, request, jsonify
+import pytesseract
+from PIL import Image
+import openai
+import os
+
+pytesseract.pytesseract.tesseract_cmd = r'/opt/homebrew/bin/tesseract'
+openai.api_key = os.getenv("OPENAI_API_KEY", "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")
+app = Flask(__name__)
+
+def generate_response(extracted_text, prompt):
+    full_prompt = f"{extracted_text} {prompt}"
+    
+    response = openai.Completion.create(
+        engine="text-davinci-002",
+        prompt=full_prompt,
+        max_tokens=150,
+        n=1,
+        stop=None,
+        temperature=0.5,
+    )
+
+    message = response.choices[0].text.strip()
+    return message
+
+@app.route('/ocr', methods=['POST'])
+def ocr():
+    file = request.files['image']
+    img = Image.open(file.stream)
+    extracted_text = pytesseract.image_to_string(img)
+    
+    prompt = "Can you paraphrase this in a way that is organized, and easy to understand, while mantaining all of the meaning and most of the same wording?"
+    gpt_response = generate_response(extracted_text, prompt)
+
+    return jsonify({'extracted_text': extracted_text, 'gpt_response': gpt_response})
+
+if __name__ == '__main__':
+    app.run()
